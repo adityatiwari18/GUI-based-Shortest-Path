2256cf88d01f558f920ab082b0925575f2222563
khushiiguptaa <60001832+khushiiguptaa@users.noreply.github.com>
2020-02-03 14:51:22 +0530

    Update prims_algo.c

--- prims_algo.c
+++ prims_algo.c
@@ -2,14 +2,14 @@
 #include<stdlib.h>
  
 #define infinity 9999
-#define MAX 20
+#define m 10
  
-int G[MAX][MAX],spanning[MAX][MAX],n;
+int graph[m][m],spanning[m][m],n;
 int prims();
  
 int main()
 {
-	int i,j,total_cost;
+	int i,j,total_weight;
 	printf("Enter no. of vertices:");
 	scanf("%d",&n);
 	
@@ -18,10 +18,10 @@ int main()
 		{
 		for(j=0;j<n;j++)
 			{
-			scanf("%d",&G[i][j]);
+			scanf("%d",&graph[i][j]);
 			}
 		}
-	total_cost=prims();
+	total_weight=prims();
 	printf("\nspanning tree matrix:\n");
 	for(i=0;i<n;i++)
 	{
@@ -30,44 +30,41 @@ int main()
 			printf("%d\t",spanning[i][j]);
 	}
 	
-	printf("\n\nTotal cost of spanning tree= %d\n",total_cost);
+	printf("\n\nTotal weight of spanning tree= %d\n",total_weight;
 	return 0;
 }
  
 int prims()
 {
-	int cost[MAX][MAX];
-	int u,v,min_distance,distance[MAX],from[MAX];
-	int visited[MAX],no_of_edges,i,min_cost,j;
+	int weight[m][m];
+	int u,v,min_distance,distance[m],from[m];
+	int visited[m],no_of_edges,i,min_weight,j;
 	
-	//create cost[][] matrix,spanning[][]
 	for(i=0;i<n;i++)
 		for(j=0;j<n;j++)
 		{
-			if(G[i][j]==0)
-				cost[i][j]=infinity;
+			if(graph[i][j]==0)
+				weight[i][j]=infinity;
 			else
-				cost[i][j]=G[i][j];
+				weight[i][j]=graph[i][j];
 				spanning[i][j]=0;
 		}
-		
-	//initialise visited[],distance[] and from[]
+	
 	distance[0]=0;
 	visited[0]=1;
 	
 	for(i=1;i<n;i++)
 	{
-		distance[i]=cost[0][i];
+		distance[i]=weight[0][i];
 		from[i]=0;
 		visited[i]=0;
 	}
 	
-	min_cost=0;		//cost of spanning tree
+	min_weight=0;		//weight of spanning tree
 	no_of_edges=n-1;	//no. of edges to be added
 	
 	while(no_of_edges>0)
 	{
-		//find the vertex at minimum distance from the tree
 		min_distance=infinity;
 		for(i=1;i<n;i++)
 			if(visited[i]==0&&distance[i]<min_distance)
@@ -86,15 +83,15 @@ int prims()
 		
 		//updated the distance[] array
 		for(i=1;i<n;i++)
-			if(visited[i]==0&&cost[i][v]<distance[i])
+			if(visited[i]==0&&weight[i][v]<distance[i])
 			{
-				distance[i]=cost[i][v];
+				distance[i]=weight[i][v];
 				from[i]=v;
 			}
 		
-		min_cost=min_cost+cost[u][v];
+		min_weight=min_weight+weight[u][v];
 	}
 	
-	return(min_cost);
+	return(min_weight);
 }
  
